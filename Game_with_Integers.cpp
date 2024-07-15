15cbb0a4be02bf88766f5b0e931b81ebdd021998
Sushil <sushilbang17@gmail.com>
2024-07-15 16:40:55 +0530

    Added Explanation

--- Game_with_Integers.cpp
+++ Game_with_Integers.cpp
@@ -2,7 +2,16 @@
 using namespace std;
 
 #define ll long long;
-
+// Consider the remainder from dividing n
+//  by 3
+//  before the first move. If it is equal to 1
+//  or 2
+// , then Vanya can make the number n
+//  divisible by 3
+//  after the first move, i.e. he wins. Let the remainder be 0
+// , then Vanya must change the number after which it will not be divisible by 3
+// . Then Vova can do the same operation as Vanya and make it divisible by 3
+//  again. This will go on indefinitely, so Vova wins.
 void solve()
 {
     int n, a = 10;
