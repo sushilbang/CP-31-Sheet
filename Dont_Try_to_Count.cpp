d4860257e6e6dbbe8e60d6d919458d3580c98cae
Sushil <sushilbang17@gmail.com>
2024-07-16 14:47:50 +0530

    Dont_Try_to_Count.  -1<= ans <=5 (always) . Check the edge cases i.e the lengths n and m to calculate the range of answers. Also "x.find(s) != string::npos" was a new one.

---
+++ Dont_Try_to_Count.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ff first
+#define ss second
+#define pb push_back
+
+/*Note that the answer is always not greater than 5. When n=1, m=25, the answer is either 5 or −1, it is easy to see that the answer cannot be greater.
+This allows us to simply iterate over the number of operations, each time checking if soccurs in x. The time complexity of this solution is O(25⋅n⋅m).*/
+
+void solve(){
+    int n , m;
+    string x , s;
+    cin >> n >> m;
+    cin >> x >> s;
+    int i = 0;
+    int ans = -1;
+    while(i<=5){
+        if(x.find(s) != string::npos){
+            ans = i;
+            break;
+        }
+        x.append(x);
+        i++;
+    }
+    cout << ans << "\n";
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+
+    // string x = "append";
+    // x.append(x);
+    // cout << x << endl;
+    return 0;
+}
