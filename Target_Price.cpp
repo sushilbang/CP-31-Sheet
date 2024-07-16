84e3e38647973b7eeeee0abe4bece052912ebfde
Sushil <sushilbang17@gmail.com>
2024-07-16 20:10:07 +0530

    Target_Price.cpp

---
+++ Target_Price.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ff first
+#define ss second
+#define pb push_back
+
+void solve()
+{
+    int a[10][10] = {
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+        {1, 2, 3, 3, 3, 3, 3, 3, 2, 1},
+        {1, 2, 3, 4, 4, 4, 4, 3, 2, 1},
+        {1, 2, 3, 4, 5, 5, 4, 3, 2, 1},
+        {1, 2, 3, 4, 5, 5, 4, 3, 2, 1},
+        {1, 2, 3, 4, 4, 4, 4, 3, 2, 1},
+        {1, 2, 3, 3, 3, 3, 3, 3, 2, 1},
+        {1, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+
+    };
+    int points = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            char c;
+            cin >> c;
+            if (c == 'X')
+                points += a[i][j];
+        }
+    }
+    cout << points << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
